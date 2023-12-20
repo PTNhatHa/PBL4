b4936bdff4f73abb34c44fadecf1bf4551bf3c3b
hyynee04 <huyblue2504@gmail.com>
2023-12-20 10:10:44 +0700

    hy merge ha finish

--- PBL4_HAHYWU/src/main/java/controller/GrabServlet.java
+++ PBL4_HAHYWU/src/main/java/controller/GrabServlet.java
@@ -9,7 +9,6 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Properties;
 import java.util.Random;
 
